14e99776bcb08362567f26d0cf8a9868d9ec9066
Emre Yelaldı <yelaldi@outlook.com>
2023-10-16 22:03:47 +0300

    behaviour done.

---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.AddAutoMapper(assembly);
+            services.AddValidatorsFromAssembly(assembly);
+            services.AddMediatR(cfg =>
+
+                cfg.RegisterServicesFromAssembly(assembly)
+            );
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
