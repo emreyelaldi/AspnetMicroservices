5b03efe4b6a98da8c578b3d41f36684355e04105
Emre Yelaldı <yelaldi@outlook.com>
2023-10-17 00:48:07 +0300

    ..

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {
+                    UserName = "swn",
+                    FirstName = "Emre",
+                    LastName = "Yelaldı",
+                    EmailAddress = "emreyelaldi09@gmail.com",
+                    AddressLine = "Istanbul",
+                    Country = "Turkey",
+                    TotalPrice = 350 }
+            };
+        }
+    }
+}
